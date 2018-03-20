84ef121d79dc5ac78355acb0939ccff283bbfd98
Girish B <tuxinvincible@gmail.com>
2018-03-20 21:44:19 +0530

    Initial version

---
+++ cloudstreamkafka/src/main/java/com/girish/cloudstreamkafka/CloudstreamkafkaApplication.java
@@ -0,0 +1,12 @@
+package com.girish.cloudstreamkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudstreamkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudstreamkafkaApplication.class, args);
+	}
+}
